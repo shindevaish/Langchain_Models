3d75ad9481bf6074db5de5627d22db495a386a20
shindevaish <vaishnavi.230805@gmail.com>
2026-08-20 19:06:22 +0530

    embedding vector of multiple query

--- Embedding_Model/embedding_model_local.py
+++ Embedding_Model/embedding_model_local.py
@@ -2,8 +2,12 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-text = "Delhi is the capital of India."
-
-vector = embedding.embed_query(text)
+docs = {
+    "Delhi is the capital of India.",
+    "Mumbai is the capital of Maharashtra.",
+    "Kolkata is the capital of West Bengal.",
+    "Chennai is the capital of Tamil Nadu."
+}
+vector = embedding.embed_documents(docs)
 
 print(str(vector))
