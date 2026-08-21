0e14e296227e65674642fa24a8ff7e3250c42c3c
shindevaish <vaishnavi.230805@gmail.com>
2026-08-21 19:36:34 +0530

    use langchain messages to maintain history

--- Prompts/chatbot.py
+++ Prompts/chatbot.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,15 +11,18 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm=llm)
 
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful assistant."),
+]
+
 
 while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input.lower() in ["exit", "quit"]:
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print("AI: ", result.content)
 
 print(chat_history)
